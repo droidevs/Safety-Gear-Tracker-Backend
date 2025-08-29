a4d92c925fea7e1b5641cd539bc71f082ac39c10
moadomos <moadomos@gmail.com>
2025-08-29 07:55:28 +0000

    enhance app

--- src/main/java/com/droidevs/safety_gear_tracker/dto/CameraPagingRequestDto.java
+++ src/main/java/com/droidevs/safety_gear_tracker/dto/CameraPagingRequestDto.java
@@ -1,5 +1,7 @@
 package com.droidevs.safety_gear_tracker.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -7,6 +9,9 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 public class CameraPagingRequestDto extends PagingRequestDto {
     private String zone;
+    
+    @JsonProperty("management_status")
     private ManagementStatus managementStatus;
+    @JsonProperty("activation_status")
     private ActivationStatus activationStatus;
 }
